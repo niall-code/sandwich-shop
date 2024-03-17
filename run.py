30b8a82a6b40affb32d0fde93203e1f8adda4c9c
niall-code <niallcaplis@gmail.com>
2024-03-17 15:32:32 +0000

    Add validation for exactly 6 values inputted

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -15,16 +12,33 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('sandwich_data')
 
-
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Gets sale figures input from the user
     """
     print('Please enter sales data from the last market.')
     print('Data should be six numbers separated by commas.')
     print('Example: 10,20,30,40,50,60\n')
 
     data_str = input('Type your data here then press Enter: ')
-    print(f'The provided data is {data_str}')
+
+    # Convert user-provided string to a list of strings
+    sales_data = data_str.split(',')
+
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if one or more strings cannot be converted to int,
+    or if there are less or more than the expected 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f'Exactly 6 values required. {len(values)} were provided.'
+            )
+    except ValueError as e:
+        print(f'Invalid data: {e}. Please try inputting again.\n')
 
 get_sales_data()
